6142bdf2e7c98ba3a409d9fa4a9c777026d31a04
Adrianna Majewska <amajewska@sii.pl.eu>
2017-12-10 16:01:30 +0100

    change pacient

--- PatientService.java
+++ PatientService.java
@@ -5,9 +5,11 @@ class PatientService {
     private final List<Patient> patients = new ArrayList<>();
 
     PatientService() {
-        this.patients.add(new Patient("Jan", "Kowalski", 11));
         this.patients.add(new Patient("Adam", "Nowak", 12));
         this.patients.add(new Patient("Maria", "Nowakowska", 13));
+        this.patients.add(new Patient("Aaaa", "Nowakowska", 13));
+        this.patients.add(new Patient("Daaa", "Nowakowska", 13));
+        this.patients.add(new Patient("Aa", "Nowakowska", 13));
     }
 
     List<Patient> getAll() {
